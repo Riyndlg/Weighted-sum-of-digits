bcbcd21d9eff5d30459d621388c5d418aa147aaf
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-11 17:14:04 +0300

    Add project files.

---
+++ Weighted sum of digits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weighted_sum_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
