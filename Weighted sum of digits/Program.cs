80f2a976f60a6f3c395c9b12a2f51a94eccc0f38
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-11 17:43:01 +0300

    a

--- Weighted sum of digits/Program.cs
+++ Weighted sum of digits/Program.cs
@@ -10,6 +10,34 @@ namespace Weighted_sum_of_digits
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number of test-cases: ");
+            int n = int.Parse(Console.ReadLine());
+            int digitalcount;
+            int[] wsd = new int[n];
+            int[] cases = new int[n];
+            //enter data to work with
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter number for test-case: ");
+                cases[i] = int.Parse(Console.ReadLine());
+            }
+            //main cycle to calculate weighted sum of digits
+            for (int i = 0; i < n; i++)
+            {
+                digitalcount = (int)Math.Log10(cases[i]) + 1;//calculate number of digits in value
+                while (digitalcount > 0)
+                {
+                    wsd[i] += cases[i] % 10 * digitalcount;
+                    cases[i] = cases[i] / 10;
+                    digitalcount--;
+                }
+            }
+            //represent results on console
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(wsd[i] + " ");
+            }
+            Console.ReadKey();
         }
     }
 }
